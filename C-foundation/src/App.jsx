cb1c039d47001167325bd3498aee8ec4134f3e4b
sirisenju <amrohore4real@gmail.com>
2023-09-06 02:58:36 -0700

    tailwind added

---
+++ C-foundation/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './index.css'
+
+function App() {
+  return(
+    <div className="bg-slate-600 text-white">
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
